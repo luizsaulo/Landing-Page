818a2b862ed747e09f3d69ce21e7776770119450
Luiz <luizsaulo@yahoo.com.br>
2022-07-01 09:42:21 -0300

    creating the rest of the files for the components and adding the main root style file to be consumed along the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Aticle, Brand, CTA, Feature, Navbar } from './components';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>Landing Page</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
